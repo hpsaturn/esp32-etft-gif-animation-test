21bc149a55b21850f01c875eb243612386768e5a
Antonio Vanegas <hpsaturn@gmail.com>
2020-12-05 16:12:39 +0100

    changed animation for publication on Youtube

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,16 @@ TFT_eSPI tft = TFT_eSPI();
 uint_fast64_t shutdown;
 
 void setup() {
-    // put your setup code here, to run once:
     tft.init();
     tft.setRotation(1);
     tft.setSwapBytes(true);
-    tft.fillScreen(TFT_WHITE);
+    tft.fillScreen(TFT_BLACK);
 }
 
 void loop() {
+    if(shutdown++ > 5) espDeepSleep();
     for (int i = 0; i < frames; i++) {
-        delay(40);
-        tft.pushImage(60, 15, animation_width, animation_height, animation[i]);
+        tft.pushImage(2, 0, animation_width, animation_height, animation[i]);
+        delay(120);
     }
-    if(shutdown++ > 16) espDeepSleep();
 }
